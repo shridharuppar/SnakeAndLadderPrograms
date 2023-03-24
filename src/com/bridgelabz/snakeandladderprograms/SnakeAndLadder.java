eb192ae5c809fd16dd337b3cd153a900beb44304
shridharuppar <upparshridhar@gmail.com>
2023-03-25 00:01:12 +0530

    add: Snake And Ladder Progrms

---
+++ src/com/bridgelabz/snakeandladderprograms/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakeandladderprograms;
+
+public class SnakeAndLadder {
+	
+		public static void main(String[] args) {
+
+			 int player_Position=0;
+			    System.out.println("Player's starting position is: " +player_Position );
+		}
+
+	}
+
